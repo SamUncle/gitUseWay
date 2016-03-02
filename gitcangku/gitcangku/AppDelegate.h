b943f161ac6a3680bebeb9775c3d056092f5cf64
zsm <507772308@qq.com>
2016-03-02 22:21:10 +0800

    a

---
+++ gitcangku/gitcangku/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  gitcangku
+//
+//  Created by zhiyou on 16-3-2.
+//  Copyright (c) 2016年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    int a;
+}
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitcangku/gitcangku/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitcangku
+//
+//  Created by zhiyou on 16-3-2.
+//  Copyright (c) 2016年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
